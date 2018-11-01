b14bdd15fba73ebded7264a0f783e133fd522561
bingze <gooogle126@gmail.com>
2018-11-01 14:45:48 +0800

    Unchanged files

---
+++ src/components/Label.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Grid,Row,Col} from 'react-bootstrap/lib';
+import {Label} from 'react-bootstrap/lib';
+
+
+export default class LabelTag extends React.Component{
+	constructor(props){
+		super(props);
+		
+		this.PropTypes={
+			title:PropTypes.string.isRequired,
+			value:PropTypes.oneOf([PropTypes.string,PropTypes.number]).isRequired,
+			bgc:PropTypes.string.isRequired
+		}
+	}
+	
+	render(){
+		return (
+			<div>
+				<div style={{fontSize:20}}>
+					<Label style={{marginRight:-20,
+								paddingRight:20,
+								backgroundColor:this.props.bgc}}>{this.props.title}</Label>
+					<Label style={{borderLeft:'white solid',
+								borderLeftWidth:2,
+								borderTopLeftRadius:0,
+								borderBottomLeftRadius:0,
+								marginLeft:10,
+								backgroundColor:this.props.bgc}}>{this.props.value}</Label>
+				</div>
+			</div>
+		);
+	}
+}
