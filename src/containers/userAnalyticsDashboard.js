1c9d3c4c3120e9306a7726f88ff2b103104bc3e6
bingze <gooogle126@gmail.com>
2018-11-22 13:26:48 +0800

    修正userDataFirstDay算法

--- src/containers/userAnalyticsDashboard.js
+++ src/containers/userAnalyticsDashboard.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import ContainerPieChart from './containerPieChart';
-import ContainerLabelTag from './containerLabelTag';
+import * as d3 from 'd3';
 import ContainerTimeScale from './containerTimeScale';
 import ContainerLineChart from './containerLineChart';
 import Growth from '../components/Charts/growth';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from '../redux/reducers/reducers';
-import creditCard from '../components/Charts/PieChart/creditCard.json';
+import creditCard from '../components/Charts/BarChart/creditCard.json';
 import userData from '../components/Charts/LineChart/data.json';
 import ActivedUser from '../components/Charts/activedUser';
+import UserStatusTable from '../components/UserStatusTable/UserStatusTable.js';
+
 
 export default class UserAnalyticsDashboard extends React.Component{
 	constructor(){
 		super();
 		this.state={
-			store:createStore(reducer,{ color:'#0090c0',
-										data:creditCard.data,
-										userData:userData.data,
-										userDataFirstDay:userData.data.iosData[0],
-										userDataLastDay:userData.data.iosData[userData.data.iosData.length-1],
-										name:'b',
-										title:'test',
-										value:123,
-										timeScaleFilter:'week'})
+			store:createStore(reducer,{ userData:userData.data,
+										timeScaleFilter:'week',
+										beginDate:d3.timeParse("%Y-%m-%d")(userData.data.iosData[0].date),
+										endDate:d3.timeParse("%Y-%m-%d")(userData.data.iosData[userData.data.iosData.length-1].date),
+										banks:creditCard.data,
+										userDataFirstDay:userData.data.iosData[0].date,
+										userDataLastDay:userData.data.iosData[userData.data.iosData.length-1].date
+										})
 		}
 	}
 	
@@ -37,6 +37,7 @@ export default class UserAnalyticsDashboard extends React.Component{
 					<br/>
 					<Growth/>
 					<ActivedUser/>
+					<UserStatusTable/>
 				</div>
 			</Provider>
 		)
